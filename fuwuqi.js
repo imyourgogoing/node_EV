7acd889a0b0e89d0129c399111cd449e04e26c22
lishuai <554750095@qq.com>
2020-12-21 21:14:09 +0800

    2

--- fuwuqi.js
+++ fuwuqi.js
@@ -4,6 +4,10 @@ const server = http.createServer();
 
 server.on('request', (req, res) => {
     console.log('访问服务器成功')
+    const str = `您请求的url地址是${req.url},您请求的方法是${req.method}`
+    req.setHeader('Content-Type', 'text/html; charset=utf-8')
+    res.end(str)
+    res.end('lishuai')
 })
 
 server.listen('80', () => {
